ef171d868bf0b4d164de9a661ce79d6617a7093e
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-10-30 17:08:13 -0700

    Change demo app SDK versionm.

--- demo-app/app/src/main/java/com/projects/ztong/demoapp/DemoActivity.java
+++ demo-app/app/src/main/java/com/projects/ztong/demoapp/DemoActivity.java
@@ -1,24 +1,19 @@
 package com.projects.ztong.demoapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class DemoActivity extends AppCompatActivity {
+public class DemoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
 
-        Button demoButton = findViewById(R.id.demo_button);
+        Button demoButton = (Button) findViewById(R.id.demo_button);
         demoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
